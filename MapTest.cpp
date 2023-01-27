48c5530334712ea75c35587894d131d525e1ae86
Aeos <aeosdraws@gmail.com>
2023-01-26 23:59:24 -0500

    Formatting update + bug fixes

--- MapTest.cpp
+++ MapTest.cpp
@@ -1,6 +1,3 @@
-// MapTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <map>
 #include <string>
@@ -15,19 +12,8 @@ using namespace std;
 //Testing out maps in C++
 
 
-void num_check(string& input, bool &valid) {
-    for (char value : input) {
-        if (isdigit(value))
-            continue;
-        else {
-            cout << "Non number value found in string" << endl;
-            valid = false;
-            break;
-        }
-    }
-}
 
-void generateData(map<string, int> &data) {
+void generateData(map<string, int>& data) {
     //Generate loop
     cout << "Generating random values for stock... " << endl;
     this_thread::sleep_for(3000ms);
@@ -35,17 +21,19 @@ void generateData(map<string, int> &data) {
     int num{ 5 };
     int random;
     srand((unsigned)time(NULL));
-    
+
     for (int i = 0; i < num; i++) {
         //Inventory values 1-10000
         //Loop for number of values in map
-        for (auto &value : data) {
+        for (auto& value : data) {
             random = 1 + (rand() % 10000);
             value.second = random;
         }
         //cout << random << endl;
 
     }
+
+    cout << "New stock has been successfully generated!" << endl;
 }
 
 
@@ -56,13 +44,18 @@ void insertData(map<string, int>& data) {
     cout << "Enter the new item followed by the amount youd like to stock!" << endl;
     cin >> item >> number;
     data.insert({ item, number });
-    cout << "The new item " << item << " has been added with a stock of " << number << endl;
+    cout << "Creating data..." << endl;
+    this_thread::sleep_for(1000ms);
+    cout << "The new item '" << item << "' has been added with a stock of " << number << endl;
 }
 
 
 
 void displayData(map<string, int>& data) {
-    for (auto const &pair : data) {
+    cout << "Loading system data..." << endl;
+    this_thread::sleep_for(2000ms);
+    cout << "-------------------------" << endl;
+    for (auto const& pair : data) {
         cout << "The stock of " << pair.first << " is " << pair.second << endl;
     }
 }
@@ -76,65 +69,50 @@ int main()
     cout << "Welcome to the inventory data base" << endl;
     //Declare Maps
     map<string, int> storeStock{
-        
+
         {"tshirt", 0 }, {"undergarment", 0},  {"shoe", 0}, {"tie", 0}, {"sock" , 0}
 
     };
 
 
 
-    
+
     //Generate
     generateData(storeStock);
     //Menu loop
     char input{};
     do {
-        cout << "Welcome to the database!" << "\tWhat would you like to do?" << endl;
+        cout << "-------------------------" << endl;
+        cout << "This is the Aeos database system!" << "\tWhat would you like to do?" << endl;
         cout << "Type 'i' to insert new values\n";
         cout << "Type 'v' to view the data\n";
         cout << "Type 'g' to generate new values\n";
-        cout << "Or type 'q' to quit\n";
+        cout << "Or type 'q' to quit\n" << "-------------------------" << endl;
         cin >> input;
 
-        if (isupper(input)) {
-            tolower(input);
-        }
-            
+
         //Menu
         switch (input) {
-            case 'i':
-                insertData(storeStock);
-                break;
-            case 'v':
-                displayData(storeStock);
-                break;
-            case 'g':
-                cout << "The program will generate 5 new inventory values!" << endl;
-                generateData(storeStock);
-                break;
-            case 'q':
-                end = true;
-                break;
-            default:
-                cout << "Invalid input, next time use one of the values listed above" << endl;
-                break;
+        case 'i':
+            insertData(storeStock);
+            break;
+        case 'v':
+            displayData(storeStock);
+            break;
+        case 'g':
+            cout << "The program will generate 5 new inventory values!" << endl;
+            generateData(storeStock);
+            break;
+        case 'q':
+            end = true;
+            break;
+        default:
+            cout << "Invalid input, next time use one of the values listed above" << endl;
+            break;
         }
 
-        //Continue loop?
-        if (end = false) {
-            cout << "Would you like to continue?" << endl << "Please type 'y' or 'n'" << endl;
-            cin >> input;
 
 
-            if ((input == 'y') || (input == 'Y'))
-                end = false;
-            else if ((input == 'n') || (input == 'N'))
-                end = true;
-            else
-                cout << "Invalid input, returning to main menu";
-        
-        }
-    
     } while (!end);
 
     cout << "Thank you for using the Aeos database. Goodbye!";
@@ -143,5 +121,3 @@ int main()
 
 
 }
-
-
